84b044487148a74e8f1f7b503a05355e6361eed2
kesavanCgvak <kesavan@cgvak.com>
2026-04-20 14:08:18 +0530

    removed artisan command routes

--- routes/web.php
+++ routes/web.php
@@ -271,39 +271,4 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
         ->name('user-auth-events.index');
 });
 
-// Clear application cache
-Route::get('/clear-cache', function () {
-    Artisan::call('cache:clear');
-
-    return 'Application cache has been cleared';
-});
-
-// Clear route cache
-Route::get('/route-cache', function () {
-    Artisan::call('route:cache');
-
-    return 'Routes cache has been cleared';
-});
-
-// Clear config cache
-Route::get('/config-cache', function () {
-    Artisan::call('config:cache');
-
-    return 'Config cache has been cleared';
-});
-
-// Clear view cache
-Route::get('/view-clear', function () {
-    Artisan::call('view:clear');
-
-    return 'View cache has been cleared';
-});
-
-// Optimize application
-Route::get('/optimize-clear', function () {
-    Artisan::call('optimize:clear');
-
-    return 'Optimization has been cleared';
-});
-
 require __DIR__.'/auth.php';
